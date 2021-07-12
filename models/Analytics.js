b2fd472eed8c2205a46b5860a594cc714f0283e6
Paolo <devpsalud@gmail.com>
2021-07-13 02:01:10 +0800

    analytics update

---
+++ models/Analytics.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    revenue: {
+      type: String,
+      required: true
+    },
+    ordersCount: {
+      type: String,
+      required: true
+    },
+    deliveryDate: {
+      type: Date,
+      required: true
+    },
+    Price: {
+      type: Number,
+      required: true
+    },
+    userID: {
+      type: Schema.Types.ObjectId,
+      require: true,
+      ref: 'user'
+    }
+  });
+
+  module.exports = mongoose.model('analytics', UserSchema);
--- models/Order.js
+++ models/Order.js
@@ -14,6 +14,10 @@ const UserSchema = new Schema({
     type: Date,
     required: true
   },
+  purchaseDate: {
+    type: Date,
+    default: Date.now
+  },
   Price: {
     type: Number,
     required: true
